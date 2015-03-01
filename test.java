1f7117710f5aa47ffcaaeedba5b1e4d150708a77
Povilas Zitinskas <pzitinskas1@sheffield.ac.uk>
2015-03-01 20:59:10 +0000

    java commit

---
+++ test.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[]){
+		Scanner newc = new Scanner(System.in);
+		
+		int a = newc.nextInt();
+		System.out.println("Hello java"+a);
+	System.out.println("Hello java22"+a);
+	System.out.println("Hello java2233"+a);
+	System.out.println("Hello java4"+a);
+	
+	}
